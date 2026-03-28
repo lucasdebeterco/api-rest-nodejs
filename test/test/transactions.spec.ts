ba5ff5e05871f8a05869d6e27aef7b5c7d6e3b9d
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2026-03-27 23:50:33 -0300

    transactions test

--- test/test/transactions.spec.ts
+++ test/test/transactions.spec.ts
@@ -1,6 +1,6 @@
-import { afterAll, beforeAll, describe, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import request from "supertest";
-import { app } from "../src/app";
+import { app } from "../../src/app";
 
 describe("Transactions routes", () => {
   beforeAll(async () => {
@@ -20,5 +20,28 @@ describe("Transactions routes", () => {
         type: "credit",
       })
       .expect(201);
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie") || [];
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      }),
+    ]);
   });
 });
